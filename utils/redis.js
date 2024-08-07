b05bef60469d10b2d99db52e9b2540c9bc69cd93
mena5800 <mr.safwat55@gmail.com>
2024-08-08 00:32:22 +0300

    make redis client

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (error) => {
+      console.error('Redis client error:', error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) return reject(err);
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) return reject(err);
+        resolve(true);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) return reject(err);
+        resolve(true);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
